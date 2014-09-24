f93306f6ce2b793a43cd7cac3fbc9463fb4d707f
J. Harshbarger <hypercubed@gmail.com>
2014-09-24 17:56:46 +0900

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,46 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "AngularJS Markdown using CommonMark.",
+      "keywords": "$scope angularjs api class cols common-mark commonmark convert directive example function hc html js maincontroller markdown module my_markdown net ng-controller ng-model org output overview rows service span8 time"
+    },
+    {
+      "section": "api",
+      "id": "hc.commonmark.directive:commonMark",
+      "shortName": "commonMark",
+      "type": "directive",
+      "moduleName": "hc.commonmark",
+      "shortDescription": "Compiles source test into HTML.",
+      "keywords": "$scope ae api bind blank block class cols common-mark commonmark compiled compiles content defaults directive examplea exampleb examplec file function hash hc html include js maincontroller markdown module my_markdown net ng-controller ng-include ng-model options opts output override rows scope simple source span8 test text variable view works"
+    },
+    {
+      "section": "api",
+      "id": "hc.commonmark.service:commonMark",
+      "shortName": "commonMark",
+      "type": "service",
+      "moduleName": "hc.commonmark",
+      "shortDescription": "A reference to the CommonMark renderer.",
+      "keywords": "$scope $window api commonmark function hc html js maincontroller module ng-controller reference renderer service test"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "stmd.js",
+    "angular-commonmark.js",
+    "script.js"
+  ]
+};
